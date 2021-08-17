010505c3e34bb45f4a845aed6ee2ff891868ceb0
Paras gupta <43930565+parasgupta0018@users.noreply.github.com>
2021-08-17 11:44:13 +0530

    changes in schedule Meeting component

---
+++ src/components/scheduleMeeting/Tab1/Tab1.js
@@ -0,0 +1,108 @@
+import React, { useState, useEffect } from "react";
+import { db } from "../../../config/Firebase";
+import { useHistory } from "react-router-dom";
+
+function Tab1({ setZone, zone, setBooth }) {
+  const history = useHistory();
+  const [zones, setZones] = useState([]);
+  const [booths, setBooths] = useState([]);
+  useEffect(() => {
+    db.collection("booths")
+      .orderBy("boothName")
+      .get()
+      .then((snapshot) => {
+        setBooths(
+          snapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              boothName: doc.data().boothName,
+            };
+          })
+        );
+      });
+  }, []);
+
+  // useEffect(() => {
+  //   if (zone) {
+  //     db.collection("booths")
+  //       .where("zoneReference", "==", db.collection("zones").doc(zone[0]))
+  //       .get()
+  //       .then((snapshot) => {
+  //         setBooths(
+  //           snapshot.docs.map((doc) => {
+  //             return {
+  //               id: doc.id,
+  //               boothName: doc.data().boothName,
+  //             };
+  //           })
+  //         );
+  //       });
+  //     return () => {
+  //       setBooths([]);
+  //     };
+  //   }
+  // }, [zone]);
+
+  return (
+    <div className="row justify-content-center">
+      <div className="col-lg-6 col-md-8">
+        {/* <div className="form-group row cmn-form-field">
+          <label htmlFor="subcategogy" className="col-sm-4 col-form-label">
+            Choose Zone 
+          </label>
+          <div className="col-sm-8">
+            <select
+              onChange={(e) => {
+                setZone(e.target.value.split("-"));
+              }}
+              className="custom-select"
+            >
+              <option value={""}></option>
+              {zones?.map((item) => {
+                return (
+                  <option key={item.id} value={item.id + "-" + item.zoneName}>
+                    {item.zoneName}
+                  </option>
+                );
+              })}
+            </select>
+          </div>
+        </div> */}
+          <div className="form-group row cmn-form-field">
+            <label htmlFor="subcategogy" className="col-sm-4 col-form-label">
+              Choose Booth
+            </label>
+            <div className="col-sm-8">
+              <select
+                onChange={(e) => {
+                  setBooth(e.target.value.split('-'));
+                }}
+                className="custom-select"
+              >
+                <option></option>
+                {booths?.map((item) => {
+                  return (
+                    <option key={item.id} value={item.id+ "-" + item.boothName}>
+                      {item.boothName}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+          </div>
+
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            history.push("#date-time");
+          }}
+          className="register-btn"
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Tab1;
---
+++ src/components/scheduleMeeting/Tab3/Tab3.js
@@ -0,0 +1,178 @@
+import React, { useEffect, useState } from "react";
+import TimeslotTab from "../TimeslotTab/TimeslotTab";
+import { db } from "../../../config/Firebase";
+
+function Tab3({ booth, setGuest, guest, setTimeslot }) {
+  
+  const [guests, setGuests] = useState([]);
+  const [active, setActive] = useState({});
+  const [timeslots, setTimeslots] = useState(null);
+  const [navtabclass, setNavTabclass] = useState(["active", "", ""]);
+  const [tabclass, setTabclass] = useState([
+    "tab-pane fade show active",
+    "tab-pane fade",
+    "tab-pane fade",
+  ]);
+
+  useEffect(() => {
+    if(booth) {
+      db.collection("vcs")
+        .where("boothReference", "==", db.collection("booths").doc(booth[0]))
+        .get()
+        .then((snapshot) => {
+          // setGuests(
+            snapshot.docs.map((document) => {
+              // return {
+              //   id: doc.id,
+              //   guestName: doc.data().name,
+              // };
+              db.collection("slots")
+              .where("vcReference", "==", db.collection("vcs").doc(document.id))
+              .orderBy("startTime")
+              .get()
+              .then((snapshot2) => {
+                setTimeslots(
+                  snapshot2.docs.map((doc_slots) => {
+                    return {
+                      id: doc_slots.id,
+                      startTime: new Date(doc_slots.data().startTime * 1000),
+                      endTime: new Date(doc_slots.data().endTime * 1000),
+                      status: doc_slots.data().status,
+                    };
+                  })
+                );
+              });
+
+            })
+          // );
+        });      
+    }
+  }, [booth]);
+
+  return (
+    <div style={{ maxHeight: "300px", overflowY: "auto", overflowX: "hidden" }}>
+      <p>
+        Scheduling Meeting with{" "}
+        <span className="guestnm">
+          {booth[1]}
+        </span>
+      </p>
+      <div className="row">
+        <div className="col-md-3">
+          <ul className="nav nav-tabs tabs-left sideways">
+            <li className={navtabclass[0]}>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setNavTabclass(["active", "", ""]);
+                  setTabclass([
+                    "tab-pane fade show active",
+                    "tab-pane fade",
+                    "tab-pane fade",
+                  ]);
+                }}
+              >
+                August 24, 2021
+              </button>
+            </li>
+            <li className={navtabclass[1]}>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setNavTabclass(["", "active", ""]);
+                  setTabclass([
+                    "tab-pane fade ",
+                    "tab-pane fade show active",
+                    "tab-pane fade",
+                  ]);
+                }}
+              >
+                August 25, 2021
+              </button>
+            </li>
+            <li className={navtabclass[2]}>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  setNavTabclass(["", "", "active"]);
+                  setTabclass([
+                    "tab-pane fade ",
+                    "tab-pane fade ",
+                    "tab-pane fade show active",
+                  ]);
+                }}
+              >
+                August 26, 2021
+              </button>
+            </li>
+          </ul>
+        </div>
+
+        <div className="col-md-9">
+          <div className="tab-content">
+            <div className={tabclass[0]} id="home-v">
+              <div className="row justify-content-center">
+                {timeslots?.map((item) => {
+                  if (item.startTime.getDate() === 18)
+                    return (
+                      <TimeslotTab
+                        key={item.id}
+                        active={item.status !== "open"}
+                        value={{
+                          startTime: item.startTime,
+                          endTime: item.endTime,
+                        }}
+                        setTimeslot={setTimeslot}
+                      />
+                    );
+                })}
+              </div>
+            </div>
+            <div className={tabclass[1]}>
+              <div className="row justify-content-center">
+                {timeslots?.map((item) => {
+                  if (item.startTime.getDate() === 19)
+                    return (
+                      <TimeslotTab
+                        key={item.id}
+                        active={item.status !== "open"}
+                        value={{
+                          startTime: item.startTime,
+                          endTime: item.endTime,
+                        }}
+                        setTimeslot={setTimeslot}
+                      />
+                    );
+                })}
+              </div>
+            </div>
+            <div className={tabclass[2]}>
+              <div className="row justify-content-center">
+                {timeslots?.map((item) => {
+                  if (item.startTime.getDate() === 26)
+                    return (
+                      <TimeslotTab
+                        key={item.id}
+                        active={item.status !== "open"}
+                        value={{
+                          startTime: item.startTime,
+                          endTime: item.endTime,
+                        }}
+                        setTimeslot={setTimeslot}
+                      />
+                    );
+                })}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <a href="#confirm-meeting" className="register-btn">
+        Next
+      </a>
+    </div>
+  );
+}
+
+export default Tab3;
